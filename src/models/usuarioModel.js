d795a6ac095e066a4dfdef9d1a4425276e63e83e
leandroSalesCotrim <deadpoolleandro@hotmail.com>
2021-11-05 00:02:43 -0300

    Adição da API para login, falta fazer funcionar no cadastro

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,34 @@
+var database = require("../database/config")
+
+function listar() {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+    var instrucao = `
+        SELECT * FROM usuario;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function entrar(nome, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", nome, senha)
+    var instrucao = `
+        SELECT * FROM usuario WHERE nome = '${nome}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrar(nome,email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, senha);
+    var instrucao = `
+        INSERT INTO usuario (nome, email, senha, tipoUsuario) VALUES ('${nome}', '${email}', '${senha}', 'usuario');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    entrar,
+    cadastrar,
+    listar,
+};
