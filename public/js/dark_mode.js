24417ca6ec4b67daefc746a8c77cbc8067d15337
LeandroSalesCotrim <leandro.cotrim@bandtec.com.br>
2024-04-17 20:01:46 -0300

    Ajuste do html e css para deixar semelhante ao figma + remoção de codigo inutilizado

--- public/js/dark_mode.js
+++ public/js/dark_mode.js
@@ -19,97 +19,85 @@ if (mode == '1') {
     dark_mode();
 }
 //FUNÇÃO QUE DEIXA O SITE NO MODO ESCURO
-function dark_mode(){
+function dark_mode() {
     //CONDIÇÃO PARA RECONHECER SE O MODO ESCURO ESTA LIGADO OU NÃO
-        document.body.style.backgroundColor = "#151515";
-        url_icon.src = "img/icones/dark-mode-light.png";
+    document.body.style.backgroundColor = "#151515";
+    url_icon.src = "img/icones/dark-mode-light.png";
 
-        if (typeof card_login_cadastro != "undefined") {
-            card_login_cadastro.style.backgroundColor = "#EB1C23";
-        }
+    if (typeof card_login_cadastro != "undefined") {
+        card_login_cadastro.style.backgroundColor = "#EB1C23";
+    }
 
-        // if (typeof video_inicio != "undefined") {
-        //     video_inicio.innerHTML = `
-        //             <video width="auto" height="70%" autoplay muted poster="img/aranha-vermelha.png" loop src="img/logo-preto.mp4">
-        //             </video>
-        //         `;
-        // }
 
-        if (typeof img1 != "undefined") {
-            if (typeof img2 != "undefined") {
-                if (typeof img3 != "undefined") {
-                    if (typeof img4 != "undefined") {
-                        if (typeof img5 != "undefined") {
-                            if (typeof img6 != "undefined") {
-                                if (typeof img7 != "undefined") {
-                                    img1.style.borderLeftColor = "#6B0D10";
-                                    img1.style.borderRightColor = "#6B0D10";
-                                    img2.style.borderLeftColor = "#6B0D10";
-                                    img2.style.borderRightColor = "#6B0D10";
-                                    img3.style.borderLeftColor = "#6B0D10";
-                                    img3.style.borderRightColor = "#6B0D10";
-                                    img4.style.borderLeftColor = "#6B0D10";
-                                    img4.style.borderRightColor = "#6B0D10";
-                                    img5.style.borderLeftColor = "#6B0D10";
-                                    img5.style.borderRightColor = "#6B0D10";
-                                    img6.style.borderLeftColor = "#6B0D10";
-                                    img6.style.borderRightColor = "#6B0D10";
-                                    img7.style.borderLeftColor = "#6B0D10";
-                                    img7.style.borderRightColor = "#6B0D10";
-                                }
+    if (typeof img1 != "undefined") {
+        if (typeof img2 != "undefined") {
+            if (typeof img3 != "undefined") {
+                if (typeof img4 != "undefined") {
+                    if (typeof img5 != "undefined") {
+                        if (typeof img6 != "undefined") {
+                            if (typeof img7 != "undefined") {
+                                img1.style.borderColor = "#6B0D10";
+                                img1.style.borderColor = "#6B0D10";
+                                img2.style.borderColor = "#6B0D10";
+                                img2.style.borderColor = "#6B0D10";
+                                img3.style.borderColor = "#6B0D10";
+                                img3.style.borderColor = "#6B0D10";
+                                img4.style.borderColor = "#6B0D10";
+                                img4.style.borderColor = "#6B0D10";
+                                img5.style.borderColor = "#6B0D10";
+                                img5.style.borderColor = "#6B0D10";
+                                img6.style.borderColor = "#6B0D10";
+                                img6.style.borderColor = "#6B0D10";
+                                img7.style.borderColor = "#6B0D10";
+                                img7.style.borderColor = "#6B0D10";
                             }
                         }
                     }
                 }
             }
         }
+    }
 
 
 
-        //LAÇO DE REPETIÇÃO QUE ALTERA A COR DA FONTE DE TODOS OS ELEMENTOS DENTRO DO VETOR "TXT"
-        //O LAÇO TERMINA NO NUMERO DE ELEMENTOS RESGATADO PELO GETELEMENTSBYCLASSNAME (.LENGTH)
-        var i = 0;
-        var j = 0;
-        var k = 0;
-        var l = 0;
-        var m = 0;
+    //LAÇO DE REPETIÇÃO QUE ALTERA A COR DA FONTE DE TODOS OS ELEMENTOS DENTRO DO VETOR "TXT"
+    //O LAÇO TERMINA NO NUMERO DE ELEMENTOS RESGATADO PELO GETELEMENTSBYCLASSNAME (.LENGTH)
+    var i = 0;
+    var j = 0;
+    var k = 0;
+    var l = 0;
+    var m = 0;
 
-        while (i < texto.length) {
-            texto[i].style.color = "#EAEAEA";
-            i++;
-        }
+    while (i < texto.length) {
+        texto[i].style.color = "#EAEAEA";
+        i++;
+    }
 
-        // while (j < inputs.length) {
-        //     inputs[j].style.backgroundColor = "#EB1C23";
-        //     j++;
-        // }
+    while (k < card_dash.length) {
+        card_dash[k].style.backgroundColor = "#C6141A";
+        desc_card[k].style.backgroundColor = "#151515B5";
 
-        while (k < card_dash.length) {
-            card_dash[k].style.backgroundColor = "#C6141A";
-            card_dash[k].style.boxShadow = "10px 10px 0px #0596EB8a";
-            desc_card[k].style.backgroundColor = "#151515B5";
+        k++;
+    }
 
-            k++;
-        }
+    while (l < btns.length) {
+        btns[l].style.backgroundColor = "#151515";
+        l++;
+    }
 
-        while (l < btns.length) {
-            btns[l].style.backgroundColor = "#151515";
-            l++;
-        }
+    while (m < btns2.length) {
+        btns2[m].style.backgroundColor = "#EAEAEA";
+        btns2[m].style.color = "#151515";
+        m++;
+    }
 
-        while (m < btns2.length) {
-            btns2[m].style.backgroundColor = "#EAEAEA";
-            btns2[m].style.color = "#151515";
-            m++;
-        }
-
-        //DEFINE O STATUS DO DARK_MODE ATIVADO E ARMAZENA
-        localStorage.setItem('status_dark', '1');
-        mode = 1;
+    //DEFINE O STATUS DO DARK_MODE ATIVADO E ARMAZENA
+    localStorage.setItem('status_dark', '1');
+    mode = 1;
 
 }
 
-function white_mode(){
+function white_mode() {
     document.body.style.backgroundColor = "#EAEAEA";
     if (typeof card_login_cadastro != "undefined") {
         card_login_cadastro.style.backgroundColor = "#151515";
@@ -126,20 +114,20 @@ function white_mode(){
                     if (typeof img5 != "undefined") {
                         if (typeof img6 != "undefined") {
                             if (typeof img7 != "undefined") {
-                                img1.style.borderLeftColor = "#151515";
-                                img1.style.borderRightColor = "#151515";
-                                img2.style.borderLeftColor = "#151515";
-                                img2.style.borderRightColor = "#151515";
-                                img3.style.borderLeftColor = "#151515";
-                                img3.style.borderRightColor = "#151515";
-                                img4.style.borderLeftColor = "#151515";
-                                img4.style.borderRightColor = "#151515";
-                                img5.style.borderLeftColor = "#151515";
-                                img5.style.borderRightColor = "#151515";
-                                img6.style.borderLeftColor = "#151515";
-                                img6.style.borderRightColor = "#151515";
-                                img7.style.borderLeftColor = "#151515";
-                                img7.style.borderRightColor = "#151515";
+                                img1.style.borderColor = "#151515";
+                                img1.style.borderColor = "#151515";
+                                img2.style.borderColor = "#151515";
+                                img2.style.borderColor = "#151515";
+                                img3.style.borderColor = "#151515";
+                                img3.style.borderColor = "#151515";
+                                img4.style.borderColor = "#151515";
+                                img4.style.borderColor = "#151515";
+                                img5.style.borderColor = "#151515";
+                                img5.style.borderColor = "#151515";
+                                img6.style.borderColor = "#151515";
+                                img6.style.borderColor = "#151515";
+                                img7.style.borderColor = "#151515";
+                                img7.style.borderColor = "#151515";
                             }
                         }
                     }
@@ -148,13 +136,6 @@ function white_mode(){
         }
     }
 
-
-    // if (typeof video_inicio != "undefined") {
-    //     video_inicio.innerHTML = `
-    //             <video width="auto" height="70%" autoplay muted poster="img/aranha-vermelha.png" loop src="img/logo.mp4">
-    //             </video>
-    //         `;
-    // }
 
     //LAÇO DE REPETIÇÃO QUE ALTERA A COR DA FONTE DE TODOS OS ELEMENTOS DENTRO DO VETOR "TXT"
     //O LAÇO TERMINA NO NUMERO DE ELEMENTOS RESGATADO PELO GETELEMENTSBYCLASSNAME (.LENGTH)
@@ -168,14 +149,9 @@ function white_mode(){
         texto[i].style.color = "#151515";
         i++;
     }
-    // while (j < inputs.length) {
-    //     inputs[j].style.backgroundColor = "#151515";
-    //     j++;
-    // }
 
     while (k < card_dash.length) {
         card_dash[k].style.backgroundColor = "#151515";
-        card_dash[k].style.boxShadow = "10px 10px 0px #eb1c238a";
         desc_card[k].style.backgroundColor = "#151515b6";
         k++;
     }
@@ -201,7 +177,7 @@ function check_dark_mode() {
 
     if (mode == '1') {
         white_mode();
-    }else{
+    } else {
         dark_mode();
     }
 
